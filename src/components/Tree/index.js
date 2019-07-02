b34aa2ee8a8ee8ecaf15645a2893ef75094ee54d
miaozilong <miao.zilong@qq.com>
2019-07-02 23:19:41 +0800

    新增了点击树节点时自动展开当前子树的功能

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -1,18 +1,56 @@
 import React, {Component} from 'react';
 import {Tree as AntTree} from 'antd';
 import {TreeNode} from '../TreeNode'
-
+import produce from 'immer'
+import _ from 'lodash'
 
 export class Tree extends AntTree {
+
+    state = {
+        expandedKeys: []
+    }
 
     componentWillMount() {
         let {treeNodeData} = this.props;
         this.treeNodeData = treeNodeData
+        // 展开的节点
+        let {expandedKeys} = this.props;
+        this.setState(produce(draft => {
+            draft.expandedKeys = expandedKeys ? expandedKeys : []
+        }))
     }
+
+    handleOnSelect = (selectedKeys, {selected: bool, selectedNodes, node, event}) => {
+        // 展开的节点中 增加当前选中的第一个节点
+        this.setState(produce(draft => {
+            draft.expandedKeys.push(selectedKeys[0])
+        }))
+        const {onSelect} = this.props;
+        if (onSelect) {
+            onSelect(selectedKeys, {selected: bool, selectedNodes, node, event});
+        }
+    }
+
+    onExpand = (expandedKeys, {expanded, node}) => {
+        debugger
+        // expandedKeys.reverse()
+        if (expanded) {
+            this.setState(produce(draft => {
+                draft.expandedKeys.push(_.get(node, 'props.eventKey'))
+            }))
+        } else {
+            this.setState(produce(draft => {
+                draft.expandedKeys = _.remove(draft.expandedKeys, v => v !== _.get(node, 'props.eventKey'))
+            }))
+        }
+        const {onExpand} = this.props;
+        if (onExpand) {
+            this.onExpand(expandedKeys, {expanded, node})
+        }
+    };
 
     renderTreeNodes = (data, parent) =>
         data.map(item => {
-            debugger
             if (!item.isLeaf && item.children) {
                 return (
                     <TreeNode {...item} dataRef={item} parent={parent ? parent : ''}>
@@ -24,10 +62,16 @@ export class Tree extends AntTree {
         });
 
     render() {
-        console.log(this.treeNodeData)
+        // console.log(this.treeNodeData)
         return (
             <div>
-                <AntTree {...this.props}>
+                <AntTree
+                    {...this.props}
+                    blockNode={true}
+                    onSelect={this.handleOnSelect}
+                    expandedKeys={this.state.expandedKeys}
+                    onExpand={this.onExpand}
+                >
                     {this.renderTreeNodes(this.treeNodeData)}
                 </AntTree>
             </div>
