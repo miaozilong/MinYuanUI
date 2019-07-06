2bfd68b53d8a76f7e91bb73342070055566955cb
miaozilong <miao.zilong@qq.com>
2019-07-06 16:00:22 +0800

    封装了Icon组件 哈哈  目前只有一个图标  tableEdit

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,39 @@
 export default {
+    treeShaking: true,
+    plugins: [
+        // ref: https://umijs.org/plugin/umi-plugin-react.html
+        ['umi-plugin-react', {
+            antd: true,
+            dva: false,
+            dynamicImport: false,
+            title: 'MinYuanUI',
+            dll: false,
+
+            routes: {
+                exclude: [
+                    /components\//,
+                ],
+            },
+        }],
+    ],
+    urlLoaderExcludes: [/.svg$/],
     chainWebpack(config, {webpack}) {
-        urlLoaderExcludes:[/.svg$/]
+        config.module
+            .rule('svg')
+            .test(/.svg(\?v=\d+.\d+.\d+)?$/)
+            .use([
+                {
+                    loader: 'babel-loader'
+                },
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        babel: false,
+                        icon: true
+                    }
+                }
+            ])
+            .loader(require.resolve('@svgr/webpack'));
     },
 }
 
--- src/pages/IconDemo.js
+++ src/pages/IconDemo.js
@@ -8,6 +8,7 @@ export default class IconDemo extends React.Component {
     render() {
         return (
             <Icon component='tableEdit'
+                  style={{fontSize:30}}
             />
         );
     }
